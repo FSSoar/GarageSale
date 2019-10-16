0dbf424d3512ba8959b973f12e1f42942056bbc9
Himanshu Minocha <manshu4@gmail.com>
2019-10-16 18:50:17 -0500

    login infrastructure started

---
+++ login.py
@@ -0,0 +1,41 @@
+from flask import jsonify
+import mysql.connector
+from flask_mail import Mail, Message
+from flask import make_response
+from flask import Flask, request, url_for, render_template
+from flask import Blueprint
+import API_KEYS
+
+
+login  = Blueprint('login', __name__)
+
+
+@login.route('/')
+def index(): 
+    #render HTML Here 
+    return 'hi'
+
+
+
+@login.route('/validate')
+def loginUser(): 
+
+    # cnx = mysql.connector.connect(user='root', password='RootRoot1',
+    #                               host=API_KEYS.getSQLEndPoint(),
+    #                               database='innodb')
+
+
+    # firstName = request.args.get('firstName')
+    # lastName = request.args.get('lastName')
+    # email = request.args.get('email')
+    # phoneNumber = request.args.get('phoneNumber')
+    # zipCode = request.args.get('zipCode')
+
+    # try: 
+    #     insertion = "Insert INTO Users(firstName, lastName, email,phoneNumber,zipCode  ) values(%s, %s, %s,%s, %s);"
+    #     cursor = cnx.cursor()
+    #     cursor.execute(insertion, (firstName, lastName, email, phoneNumber, zipCode ))
+    #     cnx.commit()
+    #     return jsonify({"passed": True})
+    # except: 
+    return "ERROR CREATING USER"
