acb00ed87737c0bc4ba158fb27deb7eb5dd925b0
fssoar <manshu4@gmail.com>
2019-12-02 01:30:04 -0600

    Search works

--- SQLScripts/SearchTemplate.sql
+++ SQLScripts/SearchTemplate.sql
@@ -5,14 +5,35 @@ CREATE OR REPLACE View Popularity as
 		Where Items.retailerID = 1 and Items.itemName is Not NULL and Items.itemName != "" and Items.id is Not NULL
 		Group By itemId;
 
-(Select *, "ItemName"
-From Items Left Join Popularity On Popularity.itemId = Items.id
-Where (SOUNDEX(Items.itemName) like SOUNDEX("manshu") or Items.itemName Like "%manshu%") and Items.itemName != "" and Popularity.itemId 
-order By Popularity.itemCount)
-Union
-(Select *, "Brand"
-From Items Left Join Popularity On Popularity.itemId = Items.id
-Where (SOUNDEX(Items.brandName) like SOUNDEX("dyson") or Items.brandName Like "%dyson%") and Items.itemName != "" and Popularity.itemId 
-order By Popularity.itemCount)
+Select itemId, itemName, brandName, price, count(itemId)
+From  
 
+	(
+	(Select *
+	From Items Left Join Popularity On Popularity.itemId = Items.id
+	Where (SOUNDEX(Items.itemName) like SOUNDEX("manshu") or Items.itemName Like "%manshu%") and Items.itemName != "" and Popularity.itemId 
+	order By Popularity.itemCount)
+	Union
+	(Select *
+	From Items Left Join Popularity On Popularity.itemId = Items.id
+	Where (SOUNDEX(Items.brandName) like SOUNDEX("dyson") or Items.brandName Like "%dyson%") and Items.itemName != "" and Popularity.itemId 
+	order By Popularity.itemCount)
+	Union
+	(Select *
+	From Items Left Join Popularity On Popularity.itemId = Items.id
+	Where (SOUNDEX(Items.description) like SOUNDEX("cleaner") or Items.description Like "%cleaner%") and Items.itemName != "" and Popularity.itemId 
+	order By Popularity.itemCount)
+	Union
+	(
+	Select *
+	From	(
+		Select Items.id as id, retailerID, itemName, availabiltyStartDate, availabiltyEndDate, isCurrentlyAvailable, brandName, description, categoryId, price 
+		from Metadata Left Join Items on Metadata.itemId = Items.id
+		Where (SOUNDEX(Items.description) like SOUNDEX("dyson") or Items.description Like "%dyso%")
+		) as metadata  Left Join Popularity On Popularity.itemId = metadata.id
+
+	) 
+    
+    )  as allReturned 
+Group by itemId, itemName, brandName, price
 
--- search.py
+++ search.py
@@ -9,8 +9,8 @@ import API_KEYS
 
 search = Blueprint('search', __name__)
 
-@search.route('/')
-def index(key='start'): 
+@search.route('/<userId>')
+def index(userId, key='start'): 
     # initial page before first search is made
     # TODO: get/send table info to generate filter options
     return render_template("searchResults.html", res=[key]) 
@@ -47,31 +47,124 @@ def buildQuery(form):
     return query
 
 
-@search.route('/getresults', methods=['GET','POST'])
-def searchResults():
-    if request.method == 'POST':
+
+def searchPlainText(queryStr):
+
         cnx = mysql.connector.connect(user='root', password='RootRoot1',
                                     host=API_KEYS.getSQLEndPoint(),
                                     database='innodb')
 
         try:
-            query = buildQuery(request.form)
-            print(query)
+
+            queryPopularity = """ 
+                            CREATE OR REPLACE View Popularity as
+                            Select itemId, count(itemId) as itemCount
+                            From Purchases Left Join  Items On Purchases.itemId = Items.id
+                            Where Items.retailerID = 1 and Items.itemName is Not NULL and Items.itemName != "" and Items.id is Not NULL
+                            Group By itemId;
+                            
+                            """
+
+            # cursor = cnx.cursor()
+            # cursor.execute(queryPopularity)
+            # cursor.commit()
 
             cursor = cnx.cursor()
-            cursor. execute(query)
+            cursor.execute(queryPopularity)
+            cnx.commit()
+
+            query = """
+            
+            
+                Select itemId, itemName, brandName, price, count(itemId)
+                From  
+
+                    (
+                        (Select *
+                        From Items Left Join Popularity On Popularity.itemId = Items.id
+                        Where (SOUNDEX(Items.itemName) like SOUNDEX(%s) or Items.itemName Like %s) and Items.itemName != '' and Popularity.itemId 
+                        order By Popularity.itemCount)
+                        Union
+                        (Select *
+                        From Items Left Join Popularity On Popularity.itemId = Items.id
+                        Where (SOUNDEX(Items.brandName) like SOUNDEX(%s) or Items.brandName Like %s) and Items.itemName != '' and Popularity.itemId 
+                        order By Popularity.itemCount)
+                        Union
+                        (Select *
+                        From Items Left Join Popularity On Popularity.itemId = Items.id
+                        Where (SOUNDEX(Items.description) like SOUNDEX(%s) or Items.description Like %s) and Items.itemName != '' and Popularity.itemId 
+                        order By Popularity.itemCount)
+                        Union
+                        (
+                        Select *
+                        From	(
+                            Select Items.id as id, retailerID, itemName, availabiltyStartDate, availabiltyEndDate, isCurrentlyAvailable, brandName, description, categoryId, price 
+                            from Metadata Left Join Items on Metadata.itemId = Items.id
+                            Where (SOUNDEX(Items.description) like SOUNDEX(%s) or Items.description Like %s)
+                            ) as metadata  Left Join Popularity On Popularity.itemId = metadata.id
+
+                        ) 
+                        
+                    )  as allReturned 
+                Group by itemId, itemName, brandName, price
+
+
+            
+            
+            
+            
+            
+
+                """
+
+                # query.replace()
+            print(query % (queryStr,queryStr + '%',queryStr,queryStr+ '%',queryStr,queryStr+ '%',queryStr,queryStr+ '%'))
+
+            cursor = cnx.cursor()
+            cursor. execute(query, (queryStr, queryStr,  queryStr, queryStr, queryStr, queryStr, queryStr, queryStr))
             results = cursor.fetchall()
             print(results)
             if len(results) == 0:
-                return render_template("searchResults.html", res=['none'])
+                return results
             else:
-                return render_template("searchResults.html", res=['results', results])
-
-        except:
-            print("SEARCH FAILED")
+                return results # render_template("searchResults.html", res=['results', results])
+        except Exception as e:
+            print("SEARCH FAILED ", e)
             return None
 
-    # TODO: HOW TO REDIRECT WHEN REFRESHING PAGE?? (does another post instead of resetting search)
+
+
+@search.route('/getresults/<userId>', methods=['GET','POST'])
+def searchResults(userId):
     if request.method == 'GET':
+        cnx = mysql.connector.connect(user='root', password='RootRoot1',
+                                    host=API_KEYS.getSQLEndPoint(),
+                                    database='innodb')
+
+        try:
+            # query = buildQuery(request.form)
+            # print(request.form)
+            # print(query)
+
+            # cursor = cnx.cursor()
+            # cursor. execute(query)
+            # results = cursor.fetchall()
+            # 
+            results = searchPlainText(request.args.get('search'))
+            print(results)
+            if len(results) == 0:
+                print("ZERO RES")
+                return render_template("searchResults.html", res=['none'], userId= userId)
+            else:
+                print("RESULTS ")
+                return render_template("searchResults.html", res=['results', results], userId= userId)
+
+        except Exception as e:
+            print("SEARCH FAILED  ", e)
+            return str(e)
+
+    # TODO: HOW TO REDIRECT WHEN REFRESHING PAGE?? (does another post instead of resetting search)
+    if request.method == 'POST':
         return redirect('/search/')
 
+# searchPlainText("dyson")
