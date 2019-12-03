80e0c8e124246a0a22e30c914142e25159942522
fssoar <manshu4@gmail.com>
2019-12-02 21:05:00 -0600

    Search update again

--- search.py
+++ search.py
@@ -76,38 +76,37 @@ def searchPlainText(queryStr):
             query = """
             
             
-                Select itemID, itemName, brandName, price, count(itemId)
+                Select itemId, itemName, brandName, price, count(itemId)
                 From  
 
                     (
-                        (Select *
-                        From Items Left Join Popularity On Popularity.itemId = Items.id
-                        Where (SOUNDEX(Items.itemName) like SOUNDEX(%s) or Items.itemName Like %s) and Items.itemName != '' and Items.isCurrentlyAvailable = true
-                        order By Popularity.itemCount)
-                        Union
-                        (Select *
-                        From Items Left Join Popularity On Popularity.itemId = Items.id
-                        Where (SOUNDEX(Items.brandName) like SOUNDEX(%s) or Items.brandName Like %s) and Items.itemName != '' and Items.isCurrentlyAvailable = true
-                        order By Popularity.itemCount)
-                        Union
-                        (Select *
-                        From Items Left Join Popularity On Popularity.itemId = Items.id
-                        Where (SOUNDEX(Items.description) like SOUNDEX(%s) or Items.description Like %s) and Items.itemName != '' and Items.isCurrentlyAvailable = true
-                        order By Popularity.itemCount)
-                        Union
-                        (
-                        Select *
-                        From	(
-                            Select Items.id as id, retailerID, itemName, availabiltyStartDate, availabiltyEndDate, isCurrentlyAvailable, brandName, description, categoryId, price 
-                            from Metadata Left Join Items on Metadata.itemId = Items.id
-                            Where (SOUNDEX(Items.description) like SOUNDEX(%s) or Items.description Like %s) and Items.isCurrentlyAvailable = true
-                            ) as metadata  Left Join Popularity On Popularity.itemId = metadata.id
+                    (Select *
+                    From Items Left Join Popularity On Popularity.itemId = Items.id
+                    Where (SOUNDEX(Items.itemName) like SOUNDEX(%s) or Items.itemName Like %s) and Items.itemName != "" and Popularity.itemId and Items.isCurrentlyAvailable
+                    order By Popularity.itemCount)
+                    Union
+                    (Select *
+                    From Items Left Join Popularity On Popularity.itemId = Items.id
+                    Where (SOUNDEX(Items.brandName) like SOUNDEX(%s) or Items.brandName Like %s) and Items.itemName != "" and Popularity.itemId and Items.isCurrentlyAvailable
+                    order By Popularity.itemCount)
+                    Union
+                    (Select *
+                    From Items Left Join Popularity On Popularity.itemId = Items.id
+                    Where (SOUNDEX(Items.description) like SOUNDEX(%s) or Items.description Like %s) and Items.itemName != "" and Popularity.itemId and Items.isCurrentlyAvailable
+                    order By Popularity.itemCount)
+                    Union
+                    (
+                    Select *
+                    From	(
+                        Select Items.id as id, retailerID, itemName, availabiltyStartDate, availabiltyEndDate, isCurrentlyAvailable, brandName, description, categoryId, price 
+                        from Metadata Left Join Items on Metadata.itemId = Items.id
+                        Where (SOUNDEX(Items.description) like SOUNDEX(%s) or Items.description Like %s) and Items.isCurrentlyAvailable
+                        ) as metadata  Left Join Popularity On Popularity.itemId = metadata.id
 
-                        ) 
-                        
+                    ) 
+                    
                     )  as allReturned 
                 Group by itemId, itemName, brandName, price
-
 
             
             
@@ -130,7 +129,7 @@ def searchPlainText(queryStr):
                 return results # render_template("searchResults.html", res=['results', results])
         except Exception as e:
             print("SEARCH FAILED ", e)
-            return None
+            return jsonify([])
 
 
 
